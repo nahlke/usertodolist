0dca2e92ec23d718fabe363c843a05bbf84c4429
nahlke <nils.ahlkezedach.eu>
2021-08-24 15:34:56 +0200

    Hidden passwords

--- usertodolist/forms.py
+++ usertodolist/forms.py
@@ -4,13 +4,15 @@ from usertodolist.models import RegistrationModel, ToDoListModel
 
 
 class RegistrationForm(ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    passwordrepeat = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = RegistrationModel
         fields = ["email", "username", "password", "passwordrepeat"]
 
 class SigninForm(forms.Form):
     username = forms.CharField(label='username', max_length=30)
-    password = forms.CharField(label='password', max_length=30)
+    password = forms.CharField(label='password', max_length=30, widget=forms.PasswordInput)
 
 class ToDoForm(ModelForm):
     class Meta:
